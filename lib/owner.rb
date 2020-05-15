45316afb8b294f84dcc94a98d5cc8cfc40dbf29e
Joseph Romero <joeromero47@gmail.com>
2020-05-15 01:32:47 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -61,7 +61,7 @@ class Owner
   end
   
   def list_pets
-    @pets = self.dogs + self.cats
+    self.count.
     
     return "I have #{@owner.dogs.count} dog(s), and #{@owner.cats.count} cat(s)."
   end
